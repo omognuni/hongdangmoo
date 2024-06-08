38d3e0da17fa69340aeeb600c9177a63c1ead1e6
omognuni <melbong0069@gmail.com>
2024-06-08 12:06:24 +0900

    [Test] domain service test

---
+++ src/market/models.py
@@ -0,0 +1,14 @@
+class Product:
+    pass
+
+
+class CartLine:
+    pass
+
+
+class Cart:
+    pass
+
+
+class Order:
+    pass
---
+++ src/market/tests.py
@@ -0,0 +1,29 @@
+from market.models import Product, Cart, CartLine
+
+
+def add_to_cart(cart, product, qty):
+    pass
+
+
+def remove_from_cart(cart, product, qty):
+    pass
+
+
+def test_add_product_to_cart():
+    product = Product(id="keyboard-001", name="Keyboard", available_quantity=10)
+    cart = Cart()
+
+    add_to_cart(cart, product, qty=1)
+
+    assert product.available_quantity == 9
+
+
+def test_remove_from_cart():
+    product = Product(id="keyboard-001", name="Keyboard", available_quantity=9)
+    line = CartLine(product=product, qty=2)
+    cart = Cart(products=[line])
+
+    remove_from_cart(cart, product, qty=1)
+
+    assert product.available_quantit == 10
+    assert cart.products[0].qty == 1
