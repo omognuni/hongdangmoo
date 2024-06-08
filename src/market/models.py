e112556ec0f683623bddef1f266a10e88d026b06
omognuni <melbong0069@gmail.com>
2024-06-08 13:05:40 +0900

    [Test] domain service test

--- src/market/models.py
+++ src/market/models.py
@@ -1,13 +1,29 @@
+from dataclasses import dataclass
+
+
 class Product:
-    pass
+
+    def __init__(self, id: str, name: str, price: int, available_quantity: int):
+        self.id = id
+        self.name = name
+        self.price = price
+        self.available_quantity = available_quantity
+
+    def __eq__(self, other):
+        if not isinstance(other, Product):
+            return False
+        return other.id == self.id
 
 
+@dataclass(frozen=True)
 class CartLine:
-    pass
+    product: Product
+    qty: int
 
 
 class Cart:
-    pass
+    def __init__(self, cart_lines):
+        self.cart_lines = cart_lines
 
 
 class Order:
--- src/market/tests.py
+++ src/market/tests.py
@@ -10,7 +10,9 @@ def remove_from_cart(cart, product, qty):
 
 
 def test_add_product_to_cart():
-    product = Product(id="keyboard-001", name="Keyboard", available_quantity=10)
+    product = Product(
+        id="keyboard-001", name="Keyboard", price=1000, available_quantity=10
+    )
     cart = Cart()
 
     add_to_cart(cart, product, qty=1)
@@ -19,7 +21,9 @@ def test_add_product_to_cart():
 
 
 def test_remove_from_cart():
-    product = Product(id="keyboard-001", name="Keyboard", available_quantity=9)
+    product = Product(
+        id="keyboard-001", name="Keyboard", price=1000, available_quantity=9
+    )
     line = CartLine(product=product, qty=2)
     cart = Cart(products=[line])
 
